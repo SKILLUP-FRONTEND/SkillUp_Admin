bc5f07d3a2db7209ea28da8e7293b8d3d51d57eb
gracekim527 <k07173027@gmail.com>
2025-08-28 19:20:52 +0900

    :lipstick: Style: 로그인 폼 내 에러메시지 추가 (#1)

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -15,41 +15,47 @@ export default function LoginForm() {
     <div className={styles.loginContainer}>
       <div className={styles.logoContainer}>
         <Image src={skillUpIcon} alt="logo" width={172} height={26} />
-        <p >ADMIN</p>
+        <p>ADMIN</p>
       </div>
       <div className={styles.formContainer}>
-        <div className={styles.formWrap}>
-          <div className={styles.formItem}>
-            <label className={styles.formItemLabel} htmlFor="userId">
-              UserID
-            </label>
-            <input
-              type="text"
-              id="userId"
-              className={styles.formInput}
-              placeholder="아이디를 입력해주세요."
-            />
+        <div className={styles.formContent}>
+          <div className={styles.formItemWrap}>
+            <div className={styles.formItem}>
+              <label className={styles.formItemLabel} htmlFor="userId">
+                UserID
+              </label>
+              <input
+                type="text"
+                id="userId"
+                className={styles.formInput}
+                placeholder="아이디를 입력해주세요."
+              />
+            </div>
+            <div className={styles.formItem}>
+              <label className={styles.formItemLabel} htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className={styles.formInput}
+                placeholder="비밀번호를 입력해주세요."
+              />
+            </div>
           </div>
-          <div className={styles.formItem}>
-            <label className={styles.formItemLabel} htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className={styles.formInput}
-              placeholder="비밀번호를 입력해주세요."
-            />
+          <div className={styles.errorMessage}>
+            <p>등록된 계정이 아니에요</p>
           </div>
-          <div className={styles.checkWrap}>
-            <input type="checkbox" id="autoLogin" className={styles.checkInput} />
-            <label className={styles.checkLabel} htmlFor="autoLogin">
-              자동 로그인
-            </label>
-          </div>
-          <div className={styles.buttonWrap}>
-            <button className={styles.loginButton}>로그인</button>
-          </div>
+        </div>
+
+        <div className={styles.checkWrap}>
+          <input type="checkbox" id="autoLogin" className={styles.checkInput} />
+          <label className={styles.checkLabel} htmlFor="autoLogin">
+            자동 로그인
+          </label>
+        </div>
+        <div className={styles.buttonWrap}>
+          <button className={styles.loginButton}>로그인</button>
         </div>
       </div>
     </div>
